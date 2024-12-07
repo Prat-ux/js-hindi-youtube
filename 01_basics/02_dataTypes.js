64663e5bbb2b8aacd2615e5a3fbdf5ce5857f15b
Pratyush <pratyushyadav0106@gmail.com>
2024-12-08 04:51:03 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict" //treat all JS code as newer version
+
+//alert("Hello") //  we are using nodejs, not browser
+
+console.log(3 
+    + 3) // code readability is king
+
+console.log("Pratyush")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof null); // object as output (error of javascript)
+console.log(typeof undefined) // undefined as output
