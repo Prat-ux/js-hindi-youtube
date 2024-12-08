8c2285de687c82b340ba6c6a1df2e63977853e64
Pratyush <pratyushyadav0106@gmail.com>
2024-12-09 02:53:33 +0530

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+/*
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2 == 1);
+console.log(2 != 1);
+*/
+
+
+//console.log("2" > 1);
+//console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null = 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===    ->takes equality of value as well as data types
+console.log("2" === 2);
+
+// how? problem
