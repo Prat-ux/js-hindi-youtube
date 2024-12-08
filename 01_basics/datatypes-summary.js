0f8d2199fc0303bf23fa03d5ecfa1295a3ccb9c4
Pratyush <pratyushyadav0106@gmail.com>
2024-12-09 03:42:45 +0530

    datatype summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -3,6 +3,7 @@
 // 7 types : String, Number, bolean, null, undefined, Symbol,
 // BigInt
 
+/*
 const score = 100
 const scoreValue = 100.3
 
@@ -14,9 +15,23 @@ const id = symbol('123')
 const anotherId = symbol('123')
 
 console.log(id === anotherId);
+*/
+// symbol showing error why????
 
 
 
 // Reference (Non primitive)
 
 // Array, Objects, Functions
+
+const heroes = ["shaktimaan", "naagraj", "doga"]
+let myObj = {
+    name: "pratyush",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+    
+}
+console.log(typeof myFunction);
