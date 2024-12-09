e740faa9ba71a7be77041146b8bb1cc6a067e242
Pratyush <pratyushyadav0106@gmail.com>
2024-12-10 00:23:59 +0530

    strings add

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -4,7 +4,7 @@ const repoCount = 50
 //console.log(name + repoCount + " values");
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String("pratyush-py")
+const gameName = new String("pratyush-py-com")
 /*
 console.log(gameName.__proto__);
 
@@ -15,8 +15,18 @@ console.log(gameName.toUpperCase());
 console.log(gameName.charAt(3));
 console.log(gameName.indexOf('t'));
 
-const newString = gameName.substring(0,4)
+const newString = gameName.substring(0,4) //substring doesnt take negative value starts from 0 instead
 console.log(newString);
 let anotherString = gameName.slice(-8, 4);
-console.log(newString);
+console.log(anotherString);
 
+const newStringOne = "    hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://pratyush.com/pratyush%24yadav"
+console.log(url.replace('%24', '-'))
+
+console.log(url.includes('pratyush'))
+
+console.log(gameName.split('-'));
