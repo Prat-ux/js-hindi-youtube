7b92132452ee25d06fdf1c638f8e762da48fa9b1
Pratyush <pratyushyadav0106@gmail.com>
2024-12-11 00:14:39 +0530

    scope 1

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,13 @@
+// var c = 300
+let a =300
+if(true){
+    let a = 10
+    const b = 20
+    console.log(`inner value ${a}`);
+}
+
+
+console.log(a);
+// console.log(b);
+// console.log(c);
+// node ke andar aur windows ke andar global scope different hoti hai *****
