f5d4c854706595f98fb25f793345210f74d5735b
Pratyush <pratyushyadav0106@gmail.com>
2024-12-10 23:31:34 +0530

    function with all

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -33,3 +33,32 @@ function loginUserMessage(username = "pre given atleast name here"){
 }
 
 // console.log(loginUserMessage())
+
+
+function calculateCartPrice(val1, val2, ...num1){ // here ... is rest function, it takes all the values of input
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "piyush",
+    price: 50
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+    
+} // but here if object doesnt have exact spelling for example prices instead of price then error
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));
