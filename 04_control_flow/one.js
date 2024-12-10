94934637fafa29aa2f1320410dd8ab1726de4b69
Pratyush <pratyushyadav0106@gmail.com>
2024-12-11 02:39:42 +0530

    if else switch case

---
+++ 04_control_flow/one.js
@@ -0,0 +1,33 @@
+// if
+
+if (true){
+
+}
+const isUserLogginIn = false
+const temperature = 41
+if ( temperature < 50){
+    // console.log("less than 50");
+}
+else{
+    // console.log("more than 50");
+}
+// === checks type equality as well
+// <, >, <=, >=, ==, !=, ===, !==
+
+const score = 200
+if (score > 100){
+    const power = "fly"
+   // console.log(`user power: ${power}`);
+    
+}
+// console.log(`user power: ${power}`); outside of scope
+
+
+
+const balance = 1000
+
+// if(balance > 500) console.log("test"), console.log("test2");
+// wrong practice
+
+// else if statement also
+
